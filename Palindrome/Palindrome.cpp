ffc1600988dfe3e3bab6e364b7970ae4cc35d3e7
U-BSD\413058 <413058@SHS-9YBF6X3.beaverton.k12.or.us>
2023-09-11 12:07:45 -0700

    First save 9/11

---
+++ Palindrome/Palindrome.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+//drivers code
+int main() {
+  char str[10];
+  char str2[10];
+  cin >> str;
+  cin >> str2;
+
+  cout << str << endl;
+  cout << str2 << endl;
+
+
+  return 0;
+  
+}
