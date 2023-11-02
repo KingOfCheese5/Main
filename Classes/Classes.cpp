d2c5eb45960514099cc9a04b957aff4ea55a5687
U-BSD\413058 <413058@SHS-9YBF6X3.beaverton.k12.or.us>
2023-11-02 12:20:57 -0700

    New save 11/2

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -1,48 +1,70 @@
 #include <iostream>
 #include <cstring>
-#include "Media.h"
 #include <vector>
 
 using namespace std;
-
-const int SIZE = 3;
-
-void printArrayDescriptions(Media**, int);
-void printVectorDescriptions(vector<Media*> &);
-
-//Video Games(title, year, publisher rating)
-//Music(title, artist, year duration, publisher)
-//Movies(title, director, year, duration, publisher
+void ADD(vector<Media*>* media);
 
 int main() {
-  Media* list[SIZE];
-  vector<Media*> vect;
-  char* firstDescription = new char[80];
-  strcpy(firstDescription, "Media 1: ");
-  list[0] = new Media(firstDescription);
-  char* secondDescription = new char[80];
-  strcpy(secondDescription, "Media 2: ");
-  list[1] = new Media(secondDescription);
-  char* thirdDescription = new char[80];
-  strcpy(thirdDescription, "Media 3: ");
-  list[2] = new Media(thirdDescription);
-  printArrayDescriptions(list, SIZE);
-  vect.push_back(list[0]);
-  vect.push_back(list[1]);
-  vect.push_back(list[2]);
-  printVectorDescriptions(vect);
+  bool running = true;
+  char input[81];
+  char add1[] = "ADD";
+  char search1[] = "SEARCH";
+  char delete1[] = "DELETE";
+  char quit1[] = "QUIT";
+
+  vector<Media*>* media = new vector<Media*>();
+
+  while(running == true) {
+    cout << endl;
+    cout << "Enter ADD, SEARCH, DELETE to access files." << endl;
+    cout << "Else, enter QUIT to exit program" << endl;
+    cin.clear();
+    cin.ignore(81, '\n');
+    cin.get(input, 81);
+
+    for(int i = 0; i < strlen(input); i++){
+      //Helped by Ethan Kusse, checks for non-alphabet and removes them
+      if(!isalpha(input[i])){
+        memmove(input+i, input+1+i, strlen(input)-i);
+        i--;
+      }
+    }
+    if(strcmp(input, add1) == 0) {
+      ADD(vector<Media*>* media);
+    }
+    if(strcmp(input, search1) == 0) {
+      cout << "strcmp working search" << endl;
+    }
+    if(strcmp(input, delete1) == 0) {
+      cout << "strcmp working delete" << endl;
+    }
+    if(strcmp(input, quit1) == 0) {
+      cout << "Program Exited" << endl;
+      return 0;
+    }
+
+  }
+
+}
+
+
+void ADD(vector<Media*>* media) {
+  bool running = true;
+  char input[81];
+  char movies[] = "MOVIES";
+  char music[] = "MUSIC";
+  char games[] = "VIDEOGAMES";
   
-  return 0;
-}
+  
+  while(running == true) {
+    cout << "What type of media would you like to add: VIDEOGAMES, MUSIC, or MOVIES?" << endl;
+    cin.clear();
+    cin.ignore(81, '\n');
+    cin.get(input, 81);
 
-void printArrayDescriptions(Media**newlist, int size) {
-  for (int a = 0; a > size; a++) {
-    cout << newlist[a]->getDescription() << endl;
+    cout << input << endl;
+    return 0;
   }
-}
-
-void printVectorDescriptions(vector<Media*> &newvect) {
-  for (vector<Media*>::iterator it = newvect.begin(); it != newvect.end(); it++) {
-    cout << (*it)->getDescription() << endl;
-  }
+  
 }
