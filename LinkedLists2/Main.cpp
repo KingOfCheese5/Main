d8dbfe74383c800cb4832259a939c77539020089
U-BSD\413058 <413058@SHS-9YBF6X3.beaverton.k12.or.us>
2024-01-05 12:23:29 -0800

    2nd save, added print, work on delte

--- LinkedLists2/Main.cpp
+++ LinkedLists2/Main.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 
 void add(Node* &head, Node* curr, Node* prev, Node* inputNode);
+void print(Node* head);
+void remove(Node* &head, Node* curr, Node* prev, int ID);
 
 int main() {
 
@@ -21,14 +23,15 @@ int main() {
   char deletel[7];
   char quitl[5];
 
-  cout << "Enter add, print, or delete to alter the student list" << endl;
-  cout << "If not, enter quit to exit program" << endl;
-
-  while(running == true) {
-
   Node* head = NULL;
   Node* cur;
   Node* prev;
+  
+  while(running == true) {
+    
+    cout << endl;
+    cout << "Enter add, print, or delete to alter the student list" << endl;
+    cout << "If not, enter quit to exit program" << endl;
 
     //copy over strings
     char input[81];
@@ -67,15 +70,13 @@ int main() {
       cin.get();
       inputStudent->setGPA(float1);
 
-      cin.ignore(81, '\n');
-      Node* inputNode = new Node();
-
-      add(head, head, head, inputNode);
+      Node* inputNode = new Node(inputStudent);
       
+      add(head, head, head, inputNode);
     }
     
     else if(strcmp(input, printl) == 0) {
-      cout << "print" << endl;
+      print(head);
     }
     else if(strcmp(input, deletel) == 0) {
       cout << "delete" << endl;
@@ -95,6 +96,7 @@ void add(Node* &head, Node* curr, Node* prev, Node* inputNode) {
   //set new head
   if(head == NULL) {
     head = inputNode;
+    cout << "added head" << endl;
   }
   else {
     //if input is smaller than head, place in front of head
@@ -109,13 +111,63 @@ void add(Node* &head, Node* curr, Node* prev, Node* inputNode) {
        inputNode->setNext(curr);
      }
      //end list
-     else if(curr = NULL) {
-       curr = tempNode;
-       prev->setNext(curr);
+     else if(curr->getNext() == NULL) {
+       curr->setNext(inputNode);
      }
      else {
        add(head, curr->getNext(), curr, inputNode);
      }
+     return;
   }
+  return;
 }
-     
+
+void print(Node* head) {
+
+  if(head == NULL) {
+    cout << "The list is empty" << endl;
+    return;
+  }
+  //print out the information of the head node.
+  cout << head->getStudent()->firstName << " " << head->getStudent()->lastName;
+  cout << ", " << head->getStudent()->getID() << ", " << fixed << setprecision(2) << head->getStudent()->getGPA() << endl;
+
+  //cout << head->getNext()->firstName << " " << head->getNext()->lastName;
+  //cout << ", " << head->getNext()->getID() << ", " << fixed << setprecision(2) << head->getNext()->getGPA() << endl;
+  
+  if(head->getNext() != NULL){
+    cout << "Next found" << endl;
+    print(head->getNext());
+  }
+  return;
+}
+
+void remove(Node* &head, Node* curr, Node* prev, int ID) {
+
+  //List is empty
+  if(head == NULL) {
+    cout << "The list is empty" << endl;
+    return;
+  }
+  if(curr = NULL) {
+    cout << "Could not find student" << endl;
+    return;
+  }
+  if(curr->getStudent->getID == ID) {
+
+    if(head->getID == ID) {
+      head->~Node();
+      head = NULL;
+      return;
+    }
+
+    else if(curr = head) {
+      Node* tempNode = head->getNext();
+      head->~Node();
+      head = tempNode;
+      
+    }
+    
+  }
+
+}
